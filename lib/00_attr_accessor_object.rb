48c57dc6de3ad666ab9a8ce5beaa7ae100f1540d
Andrea del Rio <adelrio@gmail.com>
2017-04-25 13:14:07 -0700

    add notes for 00

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,4 +1,10 @@
-# 
+# One of the many strengths of ruby is it's ability for 'reflection'.
+# This means ruby can read information about a class or object at rubtime
+# and use any new information to help program itself. This is called metaprogramming.
+# Some key methods for this are: class(), instance_methods(),
+# instance_variables(), send(), and define_method()
+
+# below we use define_ethod to degine the getter and setter methods.
 
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
